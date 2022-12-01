72e72a022b888a55d17ba166e45134a51243fc62
Vasu Agarwal <vasu@Vasus-MacBook-Pro.local>
2022-11-30 21:57:18 -0500

    Updated tuits schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,19 @@
 import mongoose from 'mongoose';
+
 const schema = mongoose.Schema({
+                                   userName: String,
+                                   handle: String,
+                                   time: String,
                                    tuit: String,
+                                   userImage: String,
+                                   image: String,
+                                   image_title: String,
+                                   image_description: String,
+                                   image_link: String,
                                    likes: Number,
                                    liked: Boolean,
+                                   replies: String,
+                                   retuits: String,
+                                   dislikes: Number
                                }, {collection: 'tuits'});
 export default schema;
